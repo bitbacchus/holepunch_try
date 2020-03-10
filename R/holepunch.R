0eae822f49635c86a5d7da4e07866b7fffbaf0c0
Sebastian Hanss <sebastian@hanss.info>
2020-03-10 09:41:31 +0100

    Add holepunnch stuff

--- R/holepunch.R
+++ R/holepunch.R
@@ -4,11 +4,11 @@ devtools::install_github("karthik/holepunch")
 holepunch::write_compendium_description(package = "Test analysis", 
                              description = "Sebastian's dummy analysis to show holepunch")
 
-holepunch::write_dockerfile(maintainer = "your_name") 
 # To write a Dockerfile. It will automatically pick the date of the last 
 # modified file, match it to that version of R and add it here. You can 
 # override this by passing r_date to some arbitrary date
 # (but one for which a R version exists).
+holepunch::write_dockerfile(maintainer = "Sebastian Hanß") 
 
 holepunch::generate_badge() # This generates a badge for your readme.
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<!-- badges: start -->
+  [![Launch Rstudio Binder](http://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/bitbacchus/holepunch_try/master?urlpath=rstudio)
+  <!-- badges: end -->
+  
 # holepunch_try
+
